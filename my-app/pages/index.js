0ea5d7b508f075cb0eeccbf295a0d93483ca68b8
Nikhil <longestwavelength@protonmail.com>
2022-09-22 13:08:03 +0530

    some new changes

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -125,7 +125,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by LearnWeb3 Punks
+        Made by longest &#955;
       </footer>
     </div>
   );
